878d67a7bc5f5d5432aee773a62072aa56e7f3ab
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-09-20 06:00:37 -0700

    Correction on task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,4 +3,5 @@ def uppercase(str):
     for i in len(str):
         ascii_value = ord(str[i])
         if (ascii_value <= 97 and ascii_value <= 122):
-            str[i] = chr(ascii_value - 32)
+            str_copy += chr(ascii_value - 32)
+    print("{}".format(str_copy))
