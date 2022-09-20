3ba538fd7607ff157b4f789b04d5400688f30c07
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-09-20 05:15:16 -0700

    Correction on the format method used in tasks 3 through 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,4 @@
 for i in range(0, 10):
     for j in range(0, 10):
         if (i != j and i < j):
-            print(f"{i}{j}", end=", " if not(i == 8 and j == 9) else "\n")
+            print("{}{}".format(i, j), end=", " if not(i == 8 and j == 9) else "\n")
