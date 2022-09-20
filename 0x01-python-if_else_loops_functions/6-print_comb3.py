72ff6602843e59dd187e6ccf5979ba1adf3d9a24
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-09-20 05:26:05 -0700

    Corrections on tasks 4 and 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,5 @@
 for i in range(0, 10):
     for j in range(0, 10):
         if (i != j and i < j):
-            print("{}{}".format(i, j), end=", " if not(i == 8 and j == 9) else "\n")
+            print("{}{}".format(i, j),
+                  end=", " if not(i == 8 and j == 9) else "\n")
