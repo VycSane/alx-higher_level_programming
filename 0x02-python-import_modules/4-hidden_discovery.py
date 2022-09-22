36520215075358a088f8daf798c08197fdf86f96
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-09-22 06:29:15 -0700

    Added the 4-hidden_discovery.py script

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from os import sys
+import hidden_4
+
+
+def main() -> int:
+    names = dir("hidden_4")
+    for name in names:
+        if name[:2] != '__':
+            print(name)
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
