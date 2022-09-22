2cfbf121d891af8c97c4ea27505b27617e06d3e9
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-09-22 07:48:19 -0700

    Added the 100-my_calculator.py script

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+from os import sys
+from calculator_1 import add, sub, mul, div
+
+
+def main() -> int:
+    argv = sys.argv()
+    arg_len = len(argv) - 1
+    if arg_len != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    op = argv[2]
+    if op == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif op == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif op == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif op == '/':
+        print("{} / {} = {}".format(a, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
