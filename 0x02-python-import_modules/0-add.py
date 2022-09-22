3174f3ed3dff34c72e7c7076a184cbd618c07d78
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-09-22 04:43:52 -0700

    Corrected the os module line and the if check

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import os
+from os import sys
 from add_0 import add
 def main() -> int:
     a = 1
     b = 2
     result = add(a, b)
     print("{} + {} = {}".format(a, b, result))
-if __name__ = '__main__':
+if __name__ == '__main__':
     sys.exit(main())
