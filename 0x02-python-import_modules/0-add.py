524f150f45865d5bff8be1765522af263360e738
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-09-22 04:53:31 -0700

    Updated the code to meet pycodestyle requirements

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 from os import sys
 from add_0 import add
+
+
 def main() -> int:
     a = 1
     b = 2
     result = add(a, b)
     print("{} + {} = {}".format(a, b, result))
+    return 0
+
+
 if __name__ == '__main__':
     sys.exit(main())
