b49397ce15c2f2e50c0434b2812ae867391a6c8f
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-09-22 04:40:28 -0700

    Added both the readme file and the 0-add.py file

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import os
+from add_0 import add
+def main() -> int:
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
+if __name__ = '__main__':
+    sys.exit(main())
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,3 @@
+This folder contains programming tasks on python prigramming
+
+Tasks are tests mostly based on importing modules
