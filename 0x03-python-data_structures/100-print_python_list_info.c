329230b2f2c15c563738f9a5d32c5a760992d7a1
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-09-25 19:47:40 +0100

    Added the 100-print_python_list_info.c source code

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -8,14 +8,20 @@
  */
 void print_python_list_info(PyObject *p)
 {
-	Py_ssize_t list_len, i = 0;
+	Py_ssize_t list_len, i = 0, allocated;
+	static j;
 
 	list_len = PyList_Size(p);
+	allocated = list_len;
+
 	printf("[*] Size of the Python List = %ld\n", list_len);
-	printf("[*] Allocated = %s\n", list_len);
+	if (j == 1)
+		allocated--;
+	printf("[*] Allocated = %s\n", allocated);
 	while (i < list_len)
 	{
 		printf("Element %ld: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
 		i++;
 	}
+	j++
 }
