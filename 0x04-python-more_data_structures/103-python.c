0da07fc24a702dad6ede2b39837c2abef45ee03e
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-09-28 14:36:27 +0000

    Added task 16

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <Python.h>
+
+/**
+ * print_python_list - prints info on python list
+ * @p: PyObject arg
+ */
+void print_python_list(PyObject *p)
+{
+
+}
+/**
+ * print_python_bytes - prints info on python bytes
+ * @p: PyObject arg
+ */
+void print_python_bytes(PyObject *p)
+{
+
+}
