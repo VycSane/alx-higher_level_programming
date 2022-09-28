6991fc4a7a30a1c9dbc139fb6a658a3c4bf0128e
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-09-28 05:39:09 -0700

    Added task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     strg = roman_string
-    l = len(strg) if strg not is None else 0
-    if strg is None or type(strg) != str or l == 0:
+    ln = len(strg) if strg is not None else 0
+    if strg is None or type(strg) != str or ln == 0:
         return 0
     rd = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-
-
+    tmp = list(map(lambda x: x, strg))
+    stack = []
+    res = 0
+    while len(tmp) > 0:
+        a = rd[tmp.pop()]
+        b = rd[tmp.pop()] if len(tmp) > 0 else 0
+        c = a - b if a > b else a + b
+        stack.append(c)
+    for val in stack:
+        res += val
+    return res
