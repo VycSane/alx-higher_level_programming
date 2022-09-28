7e9f4882fbd082eb65b8999a4179106cfb5eef17
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-09-28 15:51:24 +0000

    Added task 16

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -8,7 +8,9 @@
  */
 void print_python_list(PyObject *p)
 {
-
+	Py_ssize_t list_len = (((PyVarObject*)(p))->ob_size);	
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %ld\n", list_len);
 }
 /**
  * print_python_bytes - prints info on python bytes
@@ -16,5 +18,14 @@ void print_python_list(PyObject *p)
  */
 void print_python_bytes(PyObject *p)
 {
-
+	if (PyBytes_Check(p))
+	{
+		printf("[.] bytes object info\n");
+		printf("  size: %ld\n", (((PyVarObject*)(p))->ob_size));
+		printf("  trying string: %s\n", (((PyBytesObject *)(p))->ob_sval));
+	}
+	else
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+	}
 }
