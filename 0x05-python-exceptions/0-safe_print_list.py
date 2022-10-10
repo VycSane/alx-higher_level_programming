ea014eff1eeee72436ad18d2cc804281a45d499e
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-10-10 15:02:09 -0700

    Added task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    while True:
+        try:
+            print(my_list[x], end='')
+            x += 1
+        except (IndexError, TypeError):
+            break
+    print("")
+
