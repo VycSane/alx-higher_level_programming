9706b498c828c7fa5d62ba58af2cfc7a08594e56
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-10-10 15:05:30 -0700

    Added task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     while True:
         try:
-            print(my_list[x], end='')
+            print("{:d}".format(my_list[x]), end='')
             x += 1
         except (IndexError, TypeError):
             break
