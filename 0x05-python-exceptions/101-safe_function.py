329ea227c804f9e754bac6476659cfd27dafbb79
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-10-10 17:42:07 -0700

    Added task 8

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,9 +4,9 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        res = fct()
+        res = fct(*args)
         return res
     except Exception as err:
-        err_msg = "Exception: " + err + "\n"
+        err_msg = "Exception: " + str(err) + "\n"
         sys.stderr.write(err_msg)
         return None
