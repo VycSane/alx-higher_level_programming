487f71e1b93e895f69af748edd60bf08c3c381c2
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-10-10 17:25:26 -0700

    Added task 7

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError) as err:
+        err_msg = "Exception: " + str(err) + "\n"
+        sys.stderr.write(err_msg)
+        return False
