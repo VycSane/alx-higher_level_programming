ecdad5e0afc9731cffc8da33e50d212c790d14f4
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-10-10 18:36:24 -0700

    Added task 9

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if a > i:
+                Exception('Too far')
+            result += pow(a, b) / i
+        except Exception:
+            result = a + b
+            break
+    return result
+
+        
